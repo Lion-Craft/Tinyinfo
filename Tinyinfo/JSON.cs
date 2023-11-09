b64d6c57e9fd801340e26124547fc1d78088147e
Lion-Craft <frommholz0105@gmail.com>
2023-11-09 18:56:46 +0100

    Added NVAPI info to JSON Export

--- Tinyinfo/JSON.cs
+++ Tinyinfo/JSON.cs
@@ -74,6 +74,7 @@ namespace Tinyinfo
 			//	GPU (WMI)
 			output += "\"WMI GPU\":\n{";
 			int gpuNo = 0;
+			string[] manufacturer = { "3dfx", "ati" };
 			foreach (var gpu in hardwareInfo.VideoControllerList)
 			{
 				gpuNo++;
@@ -91,6 +92,7 @@ namespace Tinyinfo
 
 				output += "\"Name\": \"" + gpu.Name + "\",\n";
 				output += "\"Manufacturer\": \"" + gpu.Manufacturer + "\",\n";
+				manufacturer[gpuNo - 1] = gpu.Manufacturer;
 				output += "\"Description\": \"" + gpu.Description + "\",\n";
 
 				if (gpu.VideoModeDescription == "")
@@ -111,6 +113,91 @@ namespace Tinyinfo
 				output += "}";
 			}
 
+			output += "\n},\n";
+
+			//	GPU (NVAPI)
+			output += "\"NVAPI GPU\":\n{";
+			int nvid = 0;
+			if (manufacturer[0].ToLower() == "nvidia" || manufacturer[1].ToLower() == "nvidia")
+			{
+				foreach (var nvgpu in PhysicalGPU.GetPhysicalGPUs())
+				{
+					nvid++;
+
+					if (nvid > 1)
+					{
+						output += ",\n";
+					}
+					else
+					{
+						output += "\n";
+					}
+
+					output += "\"GPU " + (nvid - 1) + "\": \n{\n";
+
+					output += "\"Graphics Card\": \"" + nvgpu.FullName + "\",\n";
+					output += "\"Chip\": \"" + nvgpu.ArchitectInformation.ShortName + "\",\n";
+					output += "\"Cores\": \"" + nvgpu.ArchitectInformation.NumberOfCores + "\",\n";
+					output += $"\"ROPs\": \"" + nvgpu.ArchitectInformation.NumberOfROPs + "\",\n";
+					output += "\"Shader Pipelines\": \"" + nvgpu.ArchitectInformation.NumberOfShaderPipelines + "\",\n";
+					output += "\"Graphics Base Clock\": \"" + (nvgpu.BaseClockFrequencies.GraphicsClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Graphics Boost Clock\": \"" + (nvgpu.BoostClockFrequencies.GraphicsClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Memory Base Clock\": \"" + (nvgpu.BaseClockFrequencies.MemoryClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Memory Boost Clock\": \"" + (nvgpu.BoostClockFrequencies.MemoryClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Memory Bus\": \"" + nvgpu.MemoryInformation.FrameBufferBandwidth + " Bit\",\n";
+					output += "\"Physical Memory Size\": \"" + (nvgpu.MemoryInformation.PhysicalFrameBufferSizeInkB / 1000) + "\",\n";
+
+					if (nvgpu.MemoryInformation.RAMType.ToString() == "14")
+					{
+						output += "\"Memory Type\": \"GDDR6\",\n";
+					}
+					else
+					{
+						output += "\"Memory Type\": \"" + nvgpu.MemoryInformation.RAMType +"\",\n";
+					}
+
+					output += "\"Memory Manufacturer\": \"" + nvgpu.MemoryInformation.RAMMaker + "\",\n";
+					output += "\"ECC Supported\": \"" + nvgpu.ECCMemoryInformation.IsSupported + "\",\n";
+					output += "\"ECC Enabled\": \"" + nvgpu.ECCMemoryInformation.IsEnabled + "\",\n";
+					output += "\"BIOS Version\": \"" + nvgpu.Bios.VersionString + "\",\n";
+					output += "\"Bus Type\": \"" + nvgpu.BusInformation.BusType + "\",\n";
+					output += "\"PCIe Lanes\": \"" + nvgpu.BusInformation.CurrentPCIeLanes + "\",\n";
+					output += "\"AGP\": \"" + nvgpu.BusInformation.AGPInformation + "\",\n";
+
+					//	Create Fan string
+					string fan = "";
+					//	Try reading Fan Speed, if not available print not available message
+					try
+					{
+						fan = nvgpu.CoolerInformation.ToString();
+					}
+					catch (Exception ex)
+					{
+						if (ex.Message.ToLower() == "nvapi_not_supported")
+						{
+							fan = "N/A";
+						}
+						else
+						{
+							fan = ex.Message;
+						}
+					}
+					output += "\"Fan Speed\": \"" + fan + "\",\n";
+
+					//	GPU Temps
+					foreach (var sensor in nvgpu.ThermalInformation.ThermalSensors)
+					{
+						output += $"\"Temp {sensor.SensorId}\": \"" + sensor + "\",\n";
+					}
+
+					output += "\"Graphics Clock\": \"" + (nvgpu.CurrentClockFrequencies.GraphicsClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Memory Clock\": \"" + (nvgpu.CurrentClockFrequencies.MemoryClock.Frequency / 1000) + " MHz\",\n";
+					output += "\"Video Clock\": \"" + (nvgpu.CurrentClockFrequencies.VideoDecodingClock.Frequency / 1000) + " MHz\"\n";
+
+					output += "}";
+				}
+			}
+			
 			output += "\n}\n";
 
 			output += "}";
